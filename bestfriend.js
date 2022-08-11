16539e170b4085a61abd87ec2f18d4eec7a65c40
Md Hasan <mdhasan8064@gmail.com>
2022-08-11 11:55:27 +0600

    best friend

--- bestfriend.js
+++ bestfriend.js
@@ -1,8 +1,11 @@
+// Q-4 Chack Is best friend
 function isBestFriend(object1, object2) {
+
     if ((typeof object1) != 'object' || (typeof object2) != 'object') {
         return 'Please Enter Object type Input';
 
     }
+
     else if (Array.isArray(object1) == true || (Array.isArray(object2)) == true) {
         return 'Please Enter Object type Input';
     }
